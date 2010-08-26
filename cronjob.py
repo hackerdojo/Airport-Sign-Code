10f24c25d22e2591e7677ee528b060146fbc8a02
Charlie <root@signin.hackerdojo.com>
2010-08-26 19:30:42 +0000

    Bugfix

--- cronjob.py
+++ cronjob.py
@@ -8,12 +8,11 @@ import json
 import sign
 import time
 
-result = urllib.urlopen("http://signs.hackerdojo.com/api")              
-rawdata = result.read()
-result.close()
-signs = json.loads(rawdata)
-
 for i in range(1,3):                        
+  result = urllib.urlopen("http://signs.hackerdojo.com/api")              
+  rawdata = result.read()
+  result.close()
+  signs = json.loads(rawdata)
   for s in signs:
     msg = s['message']
     lines = msg.split("\r\n")
@@ -21,6 +20,5 @@ for i in range(1,3):
       sign.twoLines(s['two_digit_id'], lines[0], lines[1])
     if len(lines)==1:
       sign.oneLine(s['two_digit_id'], lines[0])
-    # print "Updated sign #" + s['two_digit_id']
   time.sleep(15)
 
