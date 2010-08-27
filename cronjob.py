72854eb94e3cdaf5330c04f3719ab87c2bed25ae
Charlie <root@signin.hackerdojo.com>
2010-08-27 17:19:16 +0000

    Option to reset all regardless

--- cronjob.py
+++ cronjob.py
@@ -7,9 +7,10 @@ import urllib
 import json
 import sign
 import time
+import sys
 
-for i in range(1,3):                        
-  result = urllib.urlopen("http://signs.hackerdojo.com/api")              
+def setup_signs(api):
+  result = urllib.urlopen("http://signs.hackerdojo.com"+api)              
   rawdata = result.read()
   result.close()
   signs = json.loads(rawdata)
@@ -20,5 +21,10 @@ for i in range(1,3):
       sign.twoLines(s['two_digit_id'], lines[0], lines[1])
     if len(lines)==1:
       sign.oneLine(s['two_digit_id'], lines[0])
-  time.sleep(15)
 
+if len(sys.argv)==2 and sys.argv[1] == "all":
+  setup_signs("/api/all")
+else:
+  for i in range(1,3):                        
+    setup_signs("/api")
+    time.sleep(15)
